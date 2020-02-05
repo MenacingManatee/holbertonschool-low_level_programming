d6d360aedda126e35411ddd231ca0bd3ea82a1da
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-05 19:47:20 +0000

    Updates 5-printf.c to be betty compliant

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * from Dora Korpar, this time using only printf.
  * Return: 0 always (ok)
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }

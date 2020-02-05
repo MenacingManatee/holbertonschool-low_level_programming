99a986fe975dc132ffc5db8f491a295999c5f007
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-05 19:54:47 +0000

    101-quote.c updated with betty compliance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,16 @@
 #include <unistd.h>
 #include <string.h>
 
-int main()
+
+/**
+ * main - Uses fwrite (unistd.h) and strlen (string.h) to print to stderr
+ * without using any version of print or puts
+ * Return: always 1
+ */
+int main(void)
 {
-	char *dora={"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"};
-	fwrite(dora,strlen(dora),1,stderr);
-	return 1;
+	char *dora = {"and that piece of art is useful\" - Dora Korpar, 2
+015-10-19\n"};
+	fwrite(dora, strlen(dora), 1, stderr);
+	return (1);
 }

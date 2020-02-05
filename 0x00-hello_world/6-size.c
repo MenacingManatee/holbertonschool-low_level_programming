0a382111d966dad0c570c874c61cf5b054b37f8a
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-05 19:49:41 +0000

    Updated 6-size.c to be betty compliant

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Uses printf and sizeof to print the size in bytes
+ * of different base data types.
+ * Return: always 0 (ok)
+ */
+int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-	return 0;
+	return (0);
 }

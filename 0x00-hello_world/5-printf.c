08cfd5e456465c8f9c281296d3396e476df02b4c
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-05 19:46:46 +0000

    Updates 5-printf.c to be betty compliant

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
-#include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Uses printf to print the next section (see 4-puts.c) of a quote
+ * from Dora Korpar, this time using only printf.
+ * Return: 0 always (ok)
+ */
 int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

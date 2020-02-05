d78943bf94ba9b318b23fd3e38ef87cce00da506
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-05 19:38:14 +0000

    Updates script to be betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main()
+/*This script simply prints to stdout the first part of a quote using puts*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

a9982b60f39cf78cbb7d97a7e7e1591c52bb2f94
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-10 23:37:15 +0000

    Fixes formatting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,39 @@
-#include <stdio.h>
-
+#include "holberton.h"
+#define SEPERATE {\
+	_putchar(',');\
+	_putchar(' ');\
+	}
 /**
- * main - 
+ * times_table - prints the multiplication table of 9
  *
- * Return: Always 0 (ok)
+ * Return: none
  */
-int main(void)
+void times_table(void)
 {
-	
-	return (0);
+	int l, h = 0;
+	int multi;
+
+	for (h = 0; h < 10; h++)
+	{
+		for (l = 0; l < 10; l++)
+		{
+			multi = l * h;
+			if (multi >= 10)
+			{
+				SEPERATE;
+				_putchar((multi / 10) + '0');
+				_putchar((multi % 10) + '0');
+			}
+			else
+			{
+				if (l != 0)
+				{
+					SEPERATE;
+					_putchar(' ');
+				}
+				_putchar(l + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }

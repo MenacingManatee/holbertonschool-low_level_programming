6ca4d7fb705b88425f46f52b3d3a0ba142812be6
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-10 17:04:29 +0000

    uses _putchar to print a string

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include "holbertonhead.h"
 
 /**
- * main - 
+ * main - prints holberton using _putchar
  *
  * Return: Always 0 (ok)
  */
 int main(void)
 {
-	
+	int i;
+	char holb[] = "Holberton";
+	int leng = sizeof(holb);
+
+	for (i = 0; i < leng; i++)
+		_putchar(holb[i]);
+	putchar('\n');
 	return (0);
 }

0c14ad1fe855ab48b17ebe8fb494ebdee56f7ca2
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 16:59:20 +0000

    Removed some lines, saving progress

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,6 +3,14 @@
 	_putchar(',');\
 	_putchar(' ');\
 	}
+#define PUTBIG {\
+	_putchar((n / 100) + '0');\
+	_putchar(((n % 100) / 10) + '0');\
+	}
+#define PUT(z) {\
+	_putchar((z * (n / 10)) + '0');\
+	_putchar((z * (n % 10)) + '0');\
+	}
 /**
  * print_to_98 - prints numbers n-98, seperated by a comma
  * @n: a number provided by the user to count from
@@ -12,13 +20,11 @@
 void print_to_98(int n)
 {
 	if (n >= 98)
-	{
 		for (; n >= 98; n--)
 		{
 			if (n >= 100)
 			{
-				_putchar((n / 100) + '0');
-				_putchar(((n % 100) / 10) + '0');
+				PUTBIG;
 			}
 			else
 				_putchar(((n % 100) / 10) + '0');
@@ -28,45 +34,31 @@ void print_to_98(int n)
 				SEPERATE;
 			}
 			else
-			{
 				_putchar('\n');
-			}
 		}
-	}
 	else
-	{
 		for (; n <= 98; n++)
 		{
 			if (n >= 10)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				PUT(1);
 			}
 			else if (n < 0)
 			{
 				_putchar('-');
 				if (n <= -10)
 				{
-					_putchar((-(n / 10)) + '0');
-					_putchar((-(n % 10)) + '0');
+					PUT(-1);
 				}
 				else
-				{
 					_putchar((-(n % 10)) + '0');
-				}
 			}
 			else
-			{
 				_putchar((n % 10) + '0');
-			}
 			if (n != 98)
 			{
 				SEPERATE;
 			}
 			else
-			{
 				_putchar('\n');
-			}
-		}
-	}
-}
+		}}

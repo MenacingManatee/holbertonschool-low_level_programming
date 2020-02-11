1c293d60df42218ece39f69c7cbe3540d44d92ab
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 20:00:52 +0000

    Uses example holberton header. Main.c tests example function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
+ * main -tests example code given by holberton
  *
  * Return: Always 0 (ok)
  */
 int main(void)
 {
-	
+	int i = 0;
+
+	positive_or_negative(0);
 	return (0);
 }

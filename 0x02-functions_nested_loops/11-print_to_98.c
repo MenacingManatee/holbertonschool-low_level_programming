8a171023d11f219d7dabf6f4192f6ea40f50ba9a
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 00:45:11 +0000

    Adds way too many if conditionals

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,72 @@
-#include <stdio.h>
-
+#include "holberton.h"
+#define SEPERATE {\
+	_putchar(',');\
+	_putchar(' ');\
+	}
 /**
- * main - 
+ * print_to_98 - prints numbers n-98, seperated by a comma
+ * @n: a number provided by the user to count from
  *
- * Return: Always 0 (ok)
+ * Return: none
  */
-int main(void)
+void print_to_98(int n)
 {
-	
-	return (0);
+	if (n >= 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if (n >= 100)
+			{
+				_putchar((n / 100) + '0');
+				_putchar(((n % 100) / 10) + '0');
+			}
+			else
+				_putchar(((n % 100) / 10) + '0');
+			_putchar((n % 10) + '0');
+			if (n != 98)
+			{
+				SEPERATE;
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else if (n <= 0)
+			{
+				_putchar('-');
+				if ( n <= -10)
+				{
+					_putchar((-(n / 10)) + '0');
+					_putchar((-(n % 10)) + '0');
+				}
+				else
+				{
+					_putchar((-(n % 10)) + '0');
+				}
+			}
+			else
+			{
+				_putchar((n % 10) + '0');
+			}
+			if (n != 98)
+			{
+				SEPERATE;
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }

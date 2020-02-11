1d9d7d74487a0d069291c8ae3bf63f0bb879a6d6
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 02:25:51 +0000

    Updated with betty compliance

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - prints the sum of all fibonacci numbers not exceeding 4 million
  *
  * Return: Always 0 (ok)
  */
 int main(void)
 {
-	
+	unsigned long crnt = 2;
+	unsigned long lst = 1;
+	unsigned long tmp;
+	unsigned long sum = 2;
+
+	for (crnt = 2; crnt < 4000000;)
+	{
+		tmp = crnt + lst;
+		lst = crnt;
+		crnt = tmp;
+		if ((crnt % 2) == 0)
+			sum += crnt;
+	}
+	printf("%lu\n", sum);
 	return (0);
 }

6ed3fc4aa9e8855daefcf319e3ea5eb85d7e48b7
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 18:54:03 +0000

    Divides by 10000 to save on digits

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - Prints the first 98 fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0 (ok)
  */
 int main(void)
 {
-	
+	long double fib1 = 1.0;
+	long double fib2 = 2.0;
+	long double tmp = 3.0;
+	int i;
+
+	printf("%.0Lf, %.0Lf, ", fib1, fib2);
+	for (i = 0; i < 90; i++)
+	{
+		tmp = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = tmp;
+		printf("%.0Lf, ", fib2);
+	}
+	fib1 /= 10000;
+	fib2 /= 10000;
+	for (; i < 99; i++)
+	{
+		tmp = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = tmp;
+		if (i != 98)
+			printf("%.0Lf, ", (fib2 * 10000));
+		else
+			printf("%.0Lf\n", (fib2 * 10000));
+	}
 	return (0);
 }

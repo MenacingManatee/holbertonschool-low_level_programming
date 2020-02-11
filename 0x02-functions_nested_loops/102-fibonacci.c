bac80875de7982bc58014bf78a1fa3bdc5f45e36
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 02:18:14 +0000

    Removed extra 4 fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - prints the first 50 fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0 (ok)
  */
 int main(void)
 {
-	
+	long unsigned int crnt = 2;
+	long unsigned int lst = 1;
+	int i;
+	long unsigned int tmp;
+
+	printf("%lu\n%lu\n", lst, crnt);
+	for (i = 0; i <= 46; i++)
+	{
+		tmp = crnt + lst;
+		lst = crnt;
+		crnt = tmp;
+		printf("%lu\n", crnt);
+	}
 	return (0);
 }

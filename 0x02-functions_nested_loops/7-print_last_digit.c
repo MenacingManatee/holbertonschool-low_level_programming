306ffe3446d092a36fc5f9f67af21505c2b76646
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 15:25:32 +0000

    Updated with betty compliaance

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_last_digit(int) - returns the value of the last digit
- *
+ * print_last_digit - returns the value of the last digit
+ * @x: Digit sent by user to be returned as absolute value
  * Return: See above
  */
 int print_last_digit(int x)

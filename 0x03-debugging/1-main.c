f3334cd36a73dbe3a39ebb7cae0721efe7c9e74e
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 20:04:49 +0000

    Removes infinite while loop resulting from no break

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * main - causes an infinite loop
+ * Return: 0
  */
+
 int main(void)
 {
-	
-	return (0);
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        /*while (i < 10)
+        {
+                putchar(i);
+		}*/
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
 }

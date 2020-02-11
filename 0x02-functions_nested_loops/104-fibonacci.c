7b9c05f8d0ff8bd7ecb48004830d27525c8c96f5
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-11 19:32:19 +0000

    Splits too-large numbers into two

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,30 +7,41 @@
  */
 int main(void)
 {
-	long double fib1 = 1.0;
-	long double fib2 = 2.0;
-	long double tmp = 3.0;
+	unsigned long fib1 = 1;
+	unsigned long fib2 = 2;
+	unsigned long tmp = 3;
+	unsigned long fib1a, fib1b, fib2a, fib2b, tmp2;
 	int i;
 
-	printf("%.0Lf, %.0Lf, ", fib1, fib2);
+	printf("%lu, %lu, ", fib1, fib2);
 	for (i = 0; i < 90; i++)
 	{
 		tmp = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = tmp;
-		printf("%.0Lf, ", fib2);
+		printf("%lu, ", fib2);
 	}
-	fib1 /= 10000;
-	fib2 /= 10000;
+	fib1a = fib1 / 1000000;
+	fib1b = fib1 % 1000000;
+	fib2a = fib2 / 1000000;
+	fib2b = fib2 % 1000000;
 	for (; i <= 97; i++)
 	{
-		tmp = fib1 + fib2;
-		fib1 = fib2;
-		fib2 = tmp;
-		if (i != 95)
-			printf("%.0Lf, ", (fib2 * 10000));
+		tmp = fib1a + fib2a;
+		tmp2 = fib1b + fib2b;
+		fib1a = fib2a;
+		fib1b = fib2b;
+		fib2a = tmp;
+		fib2b = tmp2;
+		if (fib2b > 1000000)
+		{
+			fib2b = fib2b % 1000000;
+			fib2a++;
+		}
+		if (i != 97)
+			printf("%lu%lu, ", fib2a, fib2b);
 		else
-			printf("%.0Lf\n", (fib2 * 10000));
+			printf("%lu%lu\n", fib2a, fib2b);
 	}
 	return (0);
 }

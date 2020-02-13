637e0490adfe8daca1b9cf4450398966e685b37e
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-13 16:54:57 +0000

    Uses a for loop and a nested if

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - prints 1 through 100, swapping multiples of 3 with Fizz and multiples
+ * of 5 with Buzz
  *
  * Return: Always 0 (ok)
  */
 int main(void)
 {
-	
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+		}
+		else
+			printf("%d", i);
+		putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }

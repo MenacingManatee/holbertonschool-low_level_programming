06adcf987b2f455c8ee7c8fb44ddab58b96d2958
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-13 19:05:11 +0000

    Prints any number below 10000

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
+ * print_number - prints an integer
+ *@n: integer to be printed
  * Return: Always 0 (ok)
  */
-int main(void)
+void print_number(int n)
 {
-	
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 1000)
+		_putchar((n / 1000) + '0');
+	if (n > 100)
+		_putchar(((n / 100) % 10) + '0');
+	if (n > 10)
+		_putchar(((n % 100) / 10) + '0');
+	_putchar((n % 10) + '0');
 }

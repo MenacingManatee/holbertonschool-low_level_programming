223dd5917311a552428502a2c54b6cd08d8d7767
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-18 16:28:49 +0000

    Removes unused function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -27,17 +27,3 @@ void rev_string(char *s)
 		i++;
 	}
 }
-
-/**
- * _strlen - returns the length of a string
- * @s: a string (pointer to the first letter) provided by user
- * Return: string length
- */
-int _strlen(char *s)
-{
-	int len;
-
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}

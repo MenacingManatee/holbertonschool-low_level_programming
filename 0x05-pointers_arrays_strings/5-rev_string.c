675dfe146d10adb548c9a1d511c8dcb416608dd4
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-18 16:27:55 +0000

    Max length string initialized

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <string.h>
 
 /**
  * rev_string - reverses a string
@@ -7,21 +8,36 @@
  */
 void rev_string(char *s)
 {
-	int i = 0, j = 0, k, arr[150];
+	int i = 0, j = 0, k;
+	char arr[2048];
 
 	while (s[i] != '\0')
 		i++;
 	k = i;
 	while ((i != 0) && (s[j] != '\0'))
 	{
-		arr[i] = s[j];
+		arr[i - 1] = s[j];
 		j++;
 		i--;
 	}
 	i = 0;
 	while ((arr[i] != '\0') && (i < k))
 	{
-		s[i] = arr[i + 1];
+		s[i] = arr[i];
 		i++;
 	}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a string (pointer to the first letter) provided by user
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }

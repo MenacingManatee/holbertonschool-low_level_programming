d5da59f8bcbab57ee95d0d09936b9ac2a5ab6cbb
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-20 20:35:18 +0000

    Copied from 0x04???

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
+ * print_number - prints an integer
+ *@n: integer to be printed
  * Return: Always 0 (ok)
  */
-int main(void)
+void print_number(int n)
 {
-	
-	return (0);
+	int i = 0, j = n, k;
+	unsigned int x = 10;
+	unsigned int neg = -n;
+
+	for (; j; i++)
+	{
+		j = j / 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = neg;
+	}
+	if ((n < 10) && (n > -10))
+	{
+		_putchar(n + '0');
+		return;
+	}
+	k = i;
+	for (; k - 2; k--)
+		x = x * 10;
+	for (; i != 0; i--)
+	{
+		_putchar(((n / x) % 10) + '0');
+		x = x / 10;
+	}
 }

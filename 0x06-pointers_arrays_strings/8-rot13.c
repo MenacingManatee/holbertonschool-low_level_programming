bb540b2c7120aacfb08ff2a1655a5d8e4d4351c4
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-20 18:44:23 +0000

    Same as 7, just different arrays

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,12 +1,24 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * rot13 - encodes/decodes a string into rot13
+ *@s: string to be encoded
+ * Return: string s
  */
-int main(void)
+char *rot13(char *s)
 {
-	
-	return (0);
+	int i = 0, n;
+	char chars[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[i])
+	{
+		for (n = 0; n < 52; n++)
+		{
+			if (s[i] == chars[n])
+				s[i] = rot[n];
+		}
+		i++;
+	}
+	return (s);
 }

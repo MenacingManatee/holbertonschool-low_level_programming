fc2808d8ba567249c1e8a49a5d462d1097b0a890
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-20 18:10:50 +0000

    Updates comments to betty compliance

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,16 +4,14 @@
  * _strcmp - compares two strings
  *@s1: string 1 to be compared
  *@s2: string 2 to be compared
- * Return: a positive, negative, or 0 number based on how similar the strings are
+ * Return: a positive, negative, or 0 number based on the first different char
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
-
-	while ((s1[i] != '\0' && s2[j] != '\0') && s1[i] == s2[j])
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		i++;
-		j++;
+		s1++;
+		s2++;
 	}
 	if (*s1 == *s2)
 		return (0);

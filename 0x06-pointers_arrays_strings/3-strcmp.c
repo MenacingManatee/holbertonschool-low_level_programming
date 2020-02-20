1714a12b6d85de7d4c4d494cc0499604662fe269
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-20 16:39:41 +0000

    Apparently you can just do *s1 - *s2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * _strcmp - compares two strings
+ *@s1: string 1 to be compared
+ *@s2: string 2 to be compared
+ * Return: a positive, negative, or 0 number based on how similar the strings are
  */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	
-	return (0);
+	int i = 0, j = 0;
+
+	while ((s1[i] != '\0' && s2[j] != '\0') && s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
 }

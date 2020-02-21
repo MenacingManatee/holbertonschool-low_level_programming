0c1da236d4607f48991f96dace66fedc4468e088
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-21 03:39:42 +0000

    Fixed logic to get number. Still needs to be betty fixed

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,75 +1,65 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * infinite_add - adds two numbers
- *@n1: FIrst number to add
- *@n2: Second number to add
- *@r: buffer used for storage
- *@size_r: buffer size
- * Return: Pointer to result
+ * infinite_add - converts two strings to integers and adds them.
+ * @n1: First string to add
+ * @n2: Second string to add
+ * @r: string post addition
+ * @size_r: buffer size of r
+ * Return: r
  */
-int _strlen(char *a);
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, i, j, len;
+	int k, i, j, sum = 0, carry = 0, l = 0;
+	unsigned int num1, num2;
 
-	i = _strlen(n1);
-	j = _strlen(n2);
-
-	if (i > j)
-		len = i + 1;
-	else
-		len = j + 1;
-	if (len >= size_r)
-		return (0);
-	for (; len > 0; len--)
-	{
-		if (carry == 1)
-		{
-			r[len] = 1;
-		}
-		while ((i > 0) && (j > 0))
-		{
-			if ((n1[i] - '0') + (n2[j] - '0')  + carry > 10)
-			{
-				carry++;
-				r[len] += (((n1[i] - '0') +
-						 (n2[j] - '0')) + '0');
-			}
-			else
-				r[len] += (((n1[i] - '0') + carry +
-						 (n2[j] - '0')) + '0');
-			i--;
-			j--;
-		}
-		if ((i > 0) && (j < 0))
-		{
-			r[len] += n1[i];
-			i--;
-		}
-		else if ((j > 0) && (i < 0))
-		{
-			r[len] += n2[j];
-			j--;
-		}
-	}
-	if (r[0] == '\0')
-		return (r + 1);
-	else
-		return (r);
-}
-
-/**
- *_strlen - finds the length of a string
- *@a: string to find length of
- *
- *Return: Length of string
- */
-int _strlen(char *a)
-{
-	int i = 0;
-
-	while (a[i])
+	r[0] = '\0';
+	r[1] = '\0';
+	i = 0, j = 0, k = 0;
+	while (n1[i])
 		i++;
-	return (i);
+	while (n2[j])
+		j++;
+	if (i + 2 > size_r || j + 2 > size_r)
+		return (0);
+	if (i > j)
+		k = i + 1;
+	else
+		k = j + 1;
+	r[k] = '\0';
+	while (j > 0 || i > 0)
+	{
+		num1 = 0, num2 = 0;
+		if (i > 0)
+			num1 = (n1[--i] - '0');
+		else
+			num1 = 0;
+		if (j > 0)
+			num2 = (n2[--j] - '0');
+		else
+			num2 = 0;
+		if (carry > 0)
+		{
+			sum += carry;
+			carry = 0;
+		}
+		if (num1 > 0)
+			sum += num1;
+		if (num2 > 0)
+			sum += num2;
+		if (sum > 9)
+		{
+			sum = sum - 10;
+			carry++;
+		}
+		r[--k] = (sum + '0');
+		sum = 0;
+	}
+	if (carry > 0)
+		r[0] = '1';
+	if (k == 0)
+		r[k] = '\0';
+	while (r[l] == '\0')
+		l++;
+	return (r + l);
 }

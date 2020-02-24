173d1732c86339f1e7f3bd9e8e7bb6988d4a4714
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-24 15:42:16 +0000

    Uses a bit of janky logic with a break statement

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,34 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * _strspn - Gets the length of a prefix substring
+ * @s: source string
+ * @accept: characters to accept
+ * Return: Number of bytes in the initial segment of s which consists only
+ * of bytes from accept
  */
-int main(void)
+unsigned int _strspn(char *s, char *accept)
 {
-	
-	return (0);
+	int i = 0, j = 0;
+	int flag = 0;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				flag = 1;
+				break;
+			}
+			j++;
+		}
+		if (flag == 0)
+			break;
+		else
+			flag = 0;
+		i++;
+	}
+	return (i);
 }

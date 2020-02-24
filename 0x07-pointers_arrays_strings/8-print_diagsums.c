a1c7bf19390e5a41ebee4c4e172a325a1decf0fa
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-24 16:50:45 +0000

    Removes test prints

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,24 @@
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: source array of arrays
+ * @size: size of arrays
+ * Return: void
  */
-int main(void)
+void print_diagsums(int *a, int size)
 {
-	
-	return (0);
+	int i, j, sum1, sum2;
+
+	i = j = sum1 = sum2 = 0;
+	for (; i < size; i++)
+	{
+		sum1 += a[i * (1 + size)];
+	}
+	for (j = size - 1, i = 0; j >= 0; j--, i++)
+	{
+		sum2 += a[j + (size * i)];
+	}
+	printf("%d, %d\n", sum1, sum2);
 }

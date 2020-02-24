a9e72286db0eec4bbce5217e9a6085434d25ae15
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-24 17:28:50 +0000

    Getting seg fault, break for lunch

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,12 +1,22 @@
+#include "holberton.h"
 #include <stdio.h>
-
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * set_string - sets value of a pointer to a char
+ * @s: Pointer to change value of
+ * @to: char to change value to
+ * Return: void
  */
-int main(void)
+void set_string(char **s, char *to)
 {
-	
-	return (0);
+	int i = 0, j = 0;
+
+	while (to[j])
+		j++;
+	while (i <= j)
+	{
+		printf("%c, %c\n", *(*s + i), to[i]);
+		*(s + i) = &to[i];
+		printf("%c, %c\n\n", *(*s + i), to[i]);
+		i++;
+	}
 }

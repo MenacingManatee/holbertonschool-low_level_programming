8b6732a532db6425ea11a24a2439a0f46773b599
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-24 16:14:41 +0000

    When needle[0] is found, evaluates if it continues for the entirety of needle

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,39 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * _strstr - finds needle in haystack
+ * @haystack: source string
+ * @needle: substring to find
+ * Return: pointer to beginning of located substring, or null
  */
-int main(void)
+char *_strstr(char *haystack, char *needle)
 {
-	
-	return (0);
+	int i = 0, len = 0, j = 0;;
+
+	while (needle[len])
+		len++;
+	while (*haystack)
+	{
+		i = 0;
+		if (*haystack == needle[0])
+		{
+			j = 0;
+			while (needle[i])
+			{
+				if (needle[j] == *(haystack + j))
+				{
+					j++;
+					continue;
+				}
+				else
+					break;
+			}
+		}
+		if (j >= len)
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (haystack);
 }

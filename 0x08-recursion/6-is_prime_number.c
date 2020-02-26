87492650438a4221d15ccce3914a43509c3918c4
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-26 20:01:46 +0000

    Uses a checker to recursively find if n is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#include "holberton.h"
 
+int checker(int num, int prime);
 /**
- * main - 
- *
+ * is_prime_number - checks if input integer is prime
+ * @n: number to check
  * Return: Always 0 (ok)
  */
-int main(void)
+int is_prime_number(int n)
 {
-	
-	return (0);
+	int x;
+
+	if (n == 1 || -n == 1 || n == 0)
+		return (0);
+	x = checker(2, n);
+	return(x);
+}
+int checker(int num, int prime)
+{
+	if (num * 2 > prime)
+		return (1);
+	if (prime % num == 0)
+		return (0);
+	return(checker(num + 1, prime));
 }

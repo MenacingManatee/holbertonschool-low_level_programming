9c82a724254fe4e0ce42a5738d024bb17163d581
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-26 19:48:05 +0000

    Fixed second function always returning the wrong number by having recursion in the return statement

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,32 @@
-#include <stdio.h>
+#include "holberton.h"
 
+int checker(int sqrt, int num);
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * _sqrt_recursion - returns the square root of a number
+ * @n: input number
+ * Return: Squrt of n
  */
-int main(void)
+int _sqrt_recursion(int n)
 {
-	
-	return (0);
+	int i = 0, sqrt;
+
+	sqrt = checker(i, n);
+	return (sqrt);
+}
+/**
+ * checker - checks for square root
+ * @sqrt: the current square index
+ * @ num: number to find the square of
+ *
+ * Return: the square root of base, or -1 if one can't be found
+ */
+int checker(int sqrt, int num)
+{
+	if (sqrt * sqrt == num)
+	{
+		return (sqrt);
+	}
+	if (sqrt * sqrt > num)
+		return (-1);
+	return (checker(sqrt + 1, num));
 }

de29ebe3786e68e1ed2ba5be3f398efcdf12543a
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-26 18:31:39 +0000

    Fixes cases where length is one too high

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * _strlen_recursion - uses recursion to get the length of a string
+ * @s: input string
+ * Return: size of string
  */
-int main(void)
+int _strlen_recursion(char *s)
 {
-	
-	return (0);
+	int i;
+
+	if (*s)
+	{
+		i = 0;
+		i = _strlen_recursion(s + 1);
+	}
+	if (*s == '\0')
+		i--;
+	return (i + 1);
 }

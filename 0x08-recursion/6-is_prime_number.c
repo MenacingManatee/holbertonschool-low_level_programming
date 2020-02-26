feae4a5dc5b2e61cfc132e2b1c9d70b0fbf8962a
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-26 20:03:53 +0000

    Adds function description for checker, return description for prime function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,7 +4,7 @@ int checker(int num, int prime);
 /**
  * is_prime_number - checks if input integer is prime
  * @n: number to check
- * Return: Always 0 (ok)
+ * Return: 1 or 0, if prime or not prime respectively
  */
 int is_prime_number(int n)
 {
@@ -13,13 +13,20 @@ int is_prime_number(int n)
 	if (n == 1 || -n == 1 || n == 0)
 		return (0);
 	x = checker(2, n);
-	return(x);
+	return (x);
 }
+/**
+ * checker - checks if a number [prime] is prime
+ * @num: current number using to check
+ * @prime: Number being checker
+ *
+ * Return: 0 if not prime, 1 if prime
+ */
 int checker(int num, int prime)
 {
 	if (num * 2 > prime)
 		return (1);
 	if (prime % num == 0)
 		return (0);
-	return(checker(num + 1, prime));
+	return (checker(num + 1, prime));
 }

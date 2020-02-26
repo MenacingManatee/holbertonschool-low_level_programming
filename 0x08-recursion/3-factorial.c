b51e86aa689c527718c212a519d4f602d7ae6bfa
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-26 18:40:52 +0000

    Fixes betty issue

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,13 +8,13 @@
  */
 int factorial(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (1);
-	else
+	if (n > 0)
 	{
 		n *= factorial(n - 1);
 		return (n);
 	}
+	else if (n < 0)
+		return (-1);
+	else
+		return (1);
 }

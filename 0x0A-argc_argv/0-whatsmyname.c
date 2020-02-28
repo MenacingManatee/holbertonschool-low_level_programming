f2e4296c3af2fbb739b3ba12e2c8af66268cfee4
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-28 19:04:49 +0000

    Casts argc to void to remove coompilation error

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ *
+ * Return: Always 0 (ok)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

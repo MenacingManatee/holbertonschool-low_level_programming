b04fef8b125f07c46f35de27ce88cda39ed481c8
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-28 19:41:24 +0000

    Checks if each character is a number, If it doesnt fail, adds strtol of it to running total

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "holberton.h"
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * main - adds numbers sent through argv
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 if successful, 1 if theres an error
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	int i = 0, sum = 0, j = 1;
+
+	for (j = 1; j < argc; j++)
+	{
+		for (i = 0; argv[j][i]; i++)
+			if (argv[j][i] > '9' || argv[j][i] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += strtol(argv[j], NULL, 10);
+	}
+	printf("%d\n", sum);
 	return (0);
 }

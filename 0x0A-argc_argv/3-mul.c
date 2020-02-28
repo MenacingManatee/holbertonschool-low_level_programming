d699116b97cb11b7d1cc93079322631621d7f2c4
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-28 19:27:11 +0000

    Uses strtol to get an int from the string

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 normally, 1 if error occurs
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
-	return (0);
+	int sum;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		sum = strtol(argv[1], NULL, 10) * strtol(argv[2], NULL, 10);
+		printf("%s, %s\n", argv[1], argv[2]);
+		printf("%d\n", sum);
+		return (0);
+	}
 }

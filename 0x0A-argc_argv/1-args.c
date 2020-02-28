a8f3cd514adb2a3cc59c0d109a146cf77b926006
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-28 19:10:07 +0000

    Subtracts 1 from print statement to start count at 0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
+ * main - prints the number of arguments passed to it
+ * @argc: Number of arguments passed to main
+ * @argv: contents of arguments
  * Return: Always 0 (ok)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	(void)argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }

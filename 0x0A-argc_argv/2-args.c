1a917be797b622e300248ce7b82cd84476fa845f
MenacingManatee <brett.p.davis101@gmail.com>
2020-02-28 19:12:51 +0000

    Simple for loop through the argv array

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
+ * main - Prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of the arguments received
  * Return: Always 0 (ok)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	int i = 0;
+
+	for (; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }

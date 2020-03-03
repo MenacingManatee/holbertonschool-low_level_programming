87f015d666f46528d4098bdf8e55e5617fd508d7
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 21:16:34 +0000

    Uses two malloc calls to initialize array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - 
+ * alloc_grid - allocates an empty array of arrays
+ * @width: width of integer array
+ * @height: height of integer array
  *
- * Return: Always 0 (ok)
+ * Return: NULL on failure, else pointer to 2 dimensional int array
  */
-int main(void)
+int **alloc_grid(int width, int height)
 {
-	
-	return (0);
+	int **arr = malloc(height * sizeof(int *));
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int **) * width);
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
 }

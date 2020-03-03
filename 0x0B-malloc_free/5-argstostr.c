0964623da95ae124e9ad863e0adf8d7084bf87db
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 22:27:26 +0000

    About to take a break, saving progress

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,12 +1,41 @@
+#include <stdlib.h>
+#include "holberton.h"
 #include <stdio.h>
-
 /**
- * main - 
+ * argstostr - concatenates all arguments in av
+ * @ac: Size of av
+ * @av: Argument vector
  *
- * Return: Always 0 (ok)
+ * Return: Pointer to malloced result
  */
-int main(void)
+char *argstostr(int ac, char **av)
 {
-	
-	return (0);
+	char *cat;
+	int i, j, k, n, m = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			;
+		n += j + 1;
+	}
+	n--;
+	printf("%d\n", n);
+	cat = malloc((n) * sizeof(char) + ac);
+	if (cat == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			k = (j * ac) + j + m;
+			cat[k] = av[i][j];
+			printf("%c%c\n", cat[k - 1], cat[k]);
+		}
+		m++;
+		cat[++k] = '\n';
+	}
+	return (cat);
 }

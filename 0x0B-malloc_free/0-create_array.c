44971f6e8ee6e6400344b20059f645136147b13a
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 19:42:51 +0000

    Returns NULL if memory is unable to be allocated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr = malloc(sizeof(char *) * size);
+	char *arr;
 	unsigned int i;
 
 	if (size <= 0)
-		return ('\0');
+		return (NULL);
+	if (malloc(sizeof(char *) * size) != NULL)
+		arr = (char *)malloc(sizeof(char *) * size);
 	for (i = 0; i < size; i++)
 	{
 		*(arr + i) = c;

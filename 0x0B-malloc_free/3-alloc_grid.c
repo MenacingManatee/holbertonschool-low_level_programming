0bd02c995864dcc984afb600f273aa3473c555df
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 21:57:50 +0000

    Increases malloc size

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int **arr;
 	int i, j;
 
-	arr = (int **)malloc(height * sizeof(int));
+	arr = (int **)malloc(height * sizeof(int *));
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	if (arr == NULL)

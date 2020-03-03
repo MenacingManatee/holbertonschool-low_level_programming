3c355f62ea1e66b2cc73ef3d301b1ce02e7f5d5d
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 20:12:33 +0000

    Changes logic on detecting if NULL should be returned

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-	if (malloc(sizeof(char *) * size) != NULL)
-		arr = (char *)malloc(sizeof(char *) * size);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		*(arr + i) = c;

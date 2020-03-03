fcc946620fde63994b35426a67a81ddae789827c
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 22:45:09 +0000

    Frees if returns NULL

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,12 +17,18 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	if (arr == NULL)
+		free(arr);
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			for (j = 0; j <= i; j++)
+				free(arr[j]);
+			free(arr);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;

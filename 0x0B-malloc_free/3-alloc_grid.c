d72c8156df47c01887092336d5dbe6488e7865ba
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-03 21:53:15 +0000

    Slight change to bad commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,8 +21,8 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
-			return (NULL);
+		if (arr == NULL)
+			return (NULL);xs
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;

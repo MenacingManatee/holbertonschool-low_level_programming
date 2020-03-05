28a7d804319aae2528933f7844d57abb186da046
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-05 22:10:04 +0000

    Adds first checks to remove edge cases

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - 
- *
+ * main - multiplies two numbers
+ * @argc: amount of arguments, including filename
+ * @argv: argument vector
  * Return: Always 0 (ok)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n", argv[i][j]);
+				exit(98);
+			}
+		}
+	}
 	return (0);
 }

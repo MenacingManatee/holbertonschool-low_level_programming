368d6ddd7ef9020a06a06a6e3c4198bdf9470d7f
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-05 19:08:31 +0000

    Fixes for loop logic causing string 1 to be overwritten by string 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,33 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - 
+ * string_nconcat - concatenates two strings, up to n bytes
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes to concatenate
  *
- * Return: Always 0 (ok)
+ * Return: Pointer to allocated space, or NULL
  */
-int main(void)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	
-	return (0);
+	unsigned int i, len;
+	char *cat;
+
+	if (s1 == NULL)
+		s1 = "";
+	for (len = 0; s1[len]; len++)
+		;
+	if (s2 == NULL)
+		s2 = "";
+	cat = malloc((n + len) * sizeof(char));
+	if (cat == NULL)
+		return (NULL);
+	for (i = 0; s1[i]; i++)
+	{
+		cat[i] = s1[i];
+	}
+	for (len = 0; len < n; i++, len++)
+		cat[i] = s2[len];
+	return (cat);
 }

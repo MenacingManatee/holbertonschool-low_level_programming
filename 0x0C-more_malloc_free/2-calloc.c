075ea44df1750e37f4a9110f12b118d55a938bdb
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-05 19:47:59 +0000

    Uses 0 instead of NULL

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - 
+ * _calloc - Allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: byte size of the elements
  *
- * Return: Always 0 (ok)
+ * Return: Pointer to the allocated memory, or NULL
  */
-int main(void)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
-	return (0);
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return ((void *)arr);
 }

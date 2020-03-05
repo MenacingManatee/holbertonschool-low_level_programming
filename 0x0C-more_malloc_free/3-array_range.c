667b2944b976319cbc67cfa071a8cfeb662c9628
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-05 20:16:28 +0000

    Adds check if max is smaller than min

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * array_range - creates an array of integers
+ * @min: minimum value
+ * @max: maximum value
+ * Return: Null on failure, pointer to integer array otherwise
  */
-int main(void)
+int *array_range(int min, int max)
 {
-	
-	return (0);
+	int *arr;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	arr = malloc(((max - min) + 1) * sizeof(int));
+
+	if (!arr)
+		return (NULL);
+	for (i = 0; min <= max; min++, i++)
+	{
+		arr[i] = min;
+	}
+	return (arr);
 }

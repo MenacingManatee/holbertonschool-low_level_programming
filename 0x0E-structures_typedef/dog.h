0925d4544a4a87759489d0b41fac2d9e63f4b3ac
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-09 19:03:13 +0000

    Removed unnecessry typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - info on name age and owner of a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

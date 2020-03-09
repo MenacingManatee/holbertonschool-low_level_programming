6867f81f13f72bd0b2b1f1f4ef7d8fcde0669208
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-09 20:12:35 +0000

    Stores name and owner using malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int _strlen(char *s);
+void _strcpy(char *dest, char *src);
 /**
  * new_dog - creates a new dog
  * @name: dog name
@@ -12,11 +14,44 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *mydog = malloc(sizeof(mydog));
+	char *tmpname = malloc(_strlen(name) * sizeof(char));
+	char *tmpowner = malloc(_strlen(owner) * sizeof(char));
 
+	_strcpy(tmpname, name);
+	_strcpy(tmpowner, owner);
 	if (mydog == NULL)
 		return (NULL);
-	(*mydog).name = name;
+	(*mydog).name = tmpname;
 	(*mydog).age = age;
-	(*mydog).owner = owner;
+	(*mydog).owner = tmpowner;
 	return (mydog);
+}
+/**
+ * _strlen - finds the length of an input string
+ * @s: input string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * _strcpy - copies src string to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: void
+ */
+void _strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
 }

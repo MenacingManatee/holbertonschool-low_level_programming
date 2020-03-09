155978e436a75a6a3db5ab80018b3bb1a9d858fb
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-09 19:29:49 +0000

    Prints out struct in one printf statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
- * main - 
+ * print_dog - prints all members of a dog struct
+ * @d: structure to print
  *
- * Return: Always 0 (ok)
+ * Return: void
  */
-int main(void)
+void print_dog(struct dog *d)
 {
-	
-	return (0);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age,
+	       (*d).owner);
 }

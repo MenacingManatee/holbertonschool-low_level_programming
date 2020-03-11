20369835c43b6ebae23a26905df9e3cf06aba0e0
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-11 18:32:21 +0000

    1.0 Adds check for size == 0 Adds check for array == NULL Adds check for action == NULL Adds loop through array elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * main - 
+ * array_iterator - calls function *action on all elements of array
+ * @array: array of elements to perform an action on
+ * @size: size of array
+ * @action: function to perform on all elements
  *
  * Return: Always 0 (ok)
  */
-int main(void)
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	
-	return (0);
+	unsigned int i = 0;
+
+	if (array == NULL || action == NULL || size == 0)
+		return;
+	for (; i < size; i++)
+		(*action)(array[i]);
 }

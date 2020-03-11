b1bb711045e20e1efbb448da86a534656a708c4f
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-11 18:43:11 +0000

    Changed return value Removed return of array at index Adds return of index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * main - 
+ * int_index - looks for the first index where cmp does not return 0
+ * @array: array of integers to search
+ * @size: Size of array
+ * @cmp: function to compare with
  *
- * Return: Always 0 (ok)
+ * Return: First index where cmp does not return 0
  */
-int main(void)
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	
-	return (0);
+	int i;
+
+	for (i = 0; i < size && (*cmp)(array[i]) == 0; i++)
+		;
+	return (i);
 }

32798c4a02dc9ecc21f74d4aee63e0b747192b22
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-11 20:09:43 +0000

    Adds check for if second argument is multiple characters starting with an expected character

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * main - calls the calc function using sent in arguments
@@ -12,12 +13,13 @@ int main(int argc, char *argv[])
 {
 	int (*op)(int, int) = get_op_func(argv[2]);
 
-	if (argc != 4 || op == NULL)
+	if (argc != 4 || op == NULL || argv == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][0] == '%' || argv[2][0] == '/') && (atoi(argv[3]) == 0))
+	if ((!(strcmp(argv[2], "%")) || !(strcmp(argv[2], "/"))) &&
+	    (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(98);

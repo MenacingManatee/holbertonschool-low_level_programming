5489a83efc9e44c26f1b8c10c59bcc4724f8a8c2
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-11 20:46:07 +0000

    Uses pointer arithmetic and &main to get opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - 
+ * main - prints the operation codes of its own function
  *
  * Return: Always 0 (ok)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	int bytes;
+	long i;
+	unsigned int j = 256;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	bytes = atoi(argv[1]);
+
+	for (i = 0; i < bytes - 1; i++)
+	{
+		if (*((char *)(&main + i)) % j < 10)
+			putchar('0');
+		printf("%x ", *((char *)(&main + i)) % j);
+	}
+	if (*((char *)(&main + (i + 1))) % j < 10)
+		putchar('0');
+	printf("%x\n", *((char *)(&main + (i + 1))) % j);
 	return (0);
 }

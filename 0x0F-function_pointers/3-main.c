d2d9f59cedffc78f44fc159b7a9c054a5c80d08a
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-11 20:13:34 +0000

    Adds correct error codes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,22 +13,22 @@ int main(int argc, char *argv[])
 {
 	int (*op)(int, int) = get_op_func(argv[2]);
 
-	if (argc != 4 || op == NULL || argv == NULL)
+	if (argc != 4 || argv == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((!(strcmp(argv[2], "%")) || !(strcmp(argv[2], "/"))) &&
-	    (atoi(argv[3]) == 0))
+	if (op == NULL || ((!(strcmp(argv[2], "%")) ||
+			    !(strcmp(argv[2], "/"))) && (atoi(argv[3]) == 0)))
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 	if ((atoi(argv[1]) == 0 && argv[1][0] != '0') || (atoi(argv[3]) == 0 &&
 							  argv[3][0] != '0'))
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 	printf("%d\n", (*op)(atoi(argv[1]), atoi(argv[3])));
 	return (0);

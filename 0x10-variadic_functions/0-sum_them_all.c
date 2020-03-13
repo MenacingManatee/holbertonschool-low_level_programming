703b9136f92b4ac879a0346fa7708725c5866d83
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-13 21:25:30 +0000

    Uses variadic functions to sum multiple ints Adds an initialization to 0 for sum to remove junk values Adds an if check for if there are 0 arguments Adds a for loop to go through the list of arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,12 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 /**
- * main - 
+ * sum_them_all - Finds the sum of all input ints
+ * @n: Number of variable inputs
  *
- * Return: Always 0 (ok)
+ * Return: Sum of inputs
  */
-int main(void)
+int sum_them_all(const unsigned int n, ...)
 {
-	
-	return (0);
+	int b = 0;
+	unsigned int i;
+	va_list a_list;
+
+	va_start( a_list, n );
+	if (n == 0)
+	{
+		va_end(a_list);
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		b += va_arg(a_list, int);
+	}
+	va_end (a_list);
+	return (b);
 }

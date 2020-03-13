d7f2141bfd59b1ea1a9a8561eda76395737bf21a
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-13 22:59:31 +0000

    Uses a switch statement to deal with function limitations Adds _strlen function to cut line count Updates with betty formatting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,12 +1,75 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
 #include <stdio.h>
 
+int _strlen(const char * const s);
 /**
- * main - 
+ * print_all - prints all arguments, seperated by char *separator
+ * @format: list of all types of arguments
  *
- * Return: Always 0 (ok)
+ * Return: Void
  */
-int main(void)
+void print_all(const char * const format, ...)
 {
-	
-	return (0);
+	va_list args;
+	unsigned int i = 0;
+	unsigned int len = _strlen(format);
+	char *tmp;
+
+	va_start(args, format);
+	while (i < len)
+	{
+		switch (format[i])
+		{
+		case 'c':
+		{
+			printf("%c", va_arg(args, int));
+			break;
+		}
+		case 'i':
+		{
+			printf("%d", va_arg(args, int));
+			break;
+		}
+		case 'f':
+		{
+			printf("%f", va_arg(args, double));
+			break;
+		}
+		case 's':
+		{
+			tmp = va_arg(args, char *);
+			if (tmp == NULL)
+				printf("(nil)");
+			else
+				printf("%s", tmp);
+			break;
+		}
+		default:
+		{
+			i++;
+			continue;
+		}
+		}
+		if (i < len - 1)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+	}
+	va_end(args);
+}
+/**
+ * _strlen - finds the length fo string s
+ * @s: target string
+ *
+ * Return: length of s
+ */
+int _strlen(const char * const s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
 }

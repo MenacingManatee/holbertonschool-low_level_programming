d4048dbc3f82b595b2234dd0663aa35989c06948
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-19 21:35:11 +0000

    Condensed code to improve readability

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,22 +10,11 @@
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
+	const list_t *tmp = h;
 
-	for (; h->next != 0; i++)
+	for (; tmp; tmp = tmp->next, i++)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
+		printf("[%d] %s\n", tmp->len, tmp->str);
 	}
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-	{
-		printf("[%d] %s\n", h->len, h->str);
-	}
-	return (++i);
+	return (i);
 }

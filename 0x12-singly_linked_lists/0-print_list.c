d1b8b6a4570e2bfddf505b18396b5956b1b5d3b8
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-19 07:44:20 +0000

    Removes test file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,5 +21,11 @@ size_t print_list(const list_t *h)
 		}
 		h = h->next;
 	}
-	return (i);
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+	return (++i);
 }

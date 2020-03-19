dd939ff40f001394fd3b7d22bf2d44fa6f3cbc88
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-19 08:10:34 +0000

    Fixed segfault caused by not setting head to new

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include "lists.h"
+#include <string.h>
 
 /**
- * main - 
+ * add_node - adds a node to the front of a list
+ * @head: the list head
+ * @str: string to be duplicated into the list
  *
- * Return: Always 0 (ok)
+ * Return: Address of new element, or NULL
  */
-int main(void)
+list_t *add_node(list_t **head, const char *str)
 {
-	
-	return (0);
+	int leng = 0;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	for (; *(str + leng); leng++)
+		;
+	new->str = strdup(str);
+	new->len = leng;
+	new->next = *head;
+	*head = new;
+	return (*head);
 }

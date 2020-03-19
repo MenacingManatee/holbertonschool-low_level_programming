679d61612c226e6d59c5f89310173e8f24a67c2e
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-19 08:40:37 +0000

    Properly loops through list without segfaulting

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * add_node_end - adds a node to the end of the list
+ * @head: head of the list
+ * @str: string to dupe
+ * Return: Address of new element or NULL
  */
-int main(void)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	
-	return (0);
+	int leng = 0;
+	list_t *new, *tmp;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	for (; str[leng]; leng++)
+		;
+	new->str = strdup(str);
+	new->len = leng;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		tmp = *head;
+		for (; tmp->next != NULL; leng++)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = new;
+	}
+	return (tmp);
 }

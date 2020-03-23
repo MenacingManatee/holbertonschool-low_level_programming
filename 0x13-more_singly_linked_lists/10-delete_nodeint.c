5be93534c78bc1de0d5709bc440f0fe159c13fe9
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-23 10:05:17 +0000

    Minor modifications to task 9 to delete instead of add

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * delete_nodeint_at_index - deletes a node at index idx
+ * @head: head of list
+ * @idx: index to insert node
  *
- * Return: Always 0 (ok)
+ * Return: Address of new node, or NULL
  */
-int main(void)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	
-	return (0);
+	listint_t *tmp = *head, *tmp2, *tmp3;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	for (; tmp->next != NULL && i < index - 1; i++, tmp = tmp->next)
+		;
+	tmp2 = tmp->next;
+	tmp3 = tmp2->next;
+	tmp->next = tmp3;
+	free(tmp2);
+	return (1);
 }

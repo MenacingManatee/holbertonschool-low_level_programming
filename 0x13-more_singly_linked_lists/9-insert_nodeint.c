7f9deae2ef370b65336046db556e17aa71550acb
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-23 09:57:33 +0000

    Fixed error where if idx was 0 node would be added at end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,12 +1,36 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * insert_nodeint_at_index - inserts a node at index idx
+ * @head: head of list
+ * @idx: index to insert node
+ * @n: contents of node
  *
- * Return: Always 0 (ok)
+ * Return: Address of new node, or NULL
  */
-int main(void)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	
-	return (0);
+	listint_t *new, *tmp = *head, *tmp2;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (; tmp->next != NULL && i < idx - 1; i++, tmp = tmp->next)
+		;
+	tmp2 = tmp->next;
+	new->next = tmp2;
+	tmp->next = new;
+	return (new);
 }

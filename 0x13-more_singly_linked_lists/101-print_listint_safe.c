452c5e66b58c1218106d85a0d41b5e4145a0bca7
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-23 23:14:14 +0000

    Every time the list is looped through, checks if any point starting from head is the same address

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,12 +1,35 @@
+#include "lists.h"
 #include <stdio.h>
 
 /**
- * main - 
+ * print_listint_safe - prints a listint_t list, even if it loops
+ * @head: list to print
  *
- * Return: Always 0 (ok)
+ * Return: Number of nodes
  */
-int main(void)
+size_t print_listint_safe(const listint_t *head)
 {
-	
-	return (0);
+	const listint_t *curr, *check;
+	size_t i = 0, j;
+
+	if (head == NULL)
+		exit(98);
+	curr = head;
+	while (curr)
+	{
+		check = head;
+		for (j = 0; j < i; j++)
+		{
+			if (check == curr)
+			{
+				printf("-> [%p] %d\n", curr, curr->n);
+				return (i);
+			}
+			check = check->next;
+		}
+		printf("[%p] %d\n", (void *)curr, curr->n);
+		curr = curr->next;
+		i++;
+	}
+	return (i);
 }

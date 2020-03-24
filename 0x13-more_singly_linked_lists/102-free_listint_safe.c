5ef6b784be0ed346f0a05965af950b855d3918dc
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-24 00:54:12 +0000

    Runs a check after each free to see if *h matches

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,12 +1,34 @@
+#include "lists.h"
 #include <stdio.h>
 
 /**
- * main - 
+ * free_listint_safe - prints a listint_t list, even if it loops
+ * @head: list to free
  *
- * Return: Always 0 (ok)
+ * Return: Number of nodes
  */
-int main(void)
+size_t free_listint_safe(listint_t **h)
 {
-	
-	return (0);
+	listint_t *curr = *h, *tmp;
+	int i = 0, j;
+
+	if (*h == NULL)
+		return (0);
+	for (; *h; i++)
+	{
+		tmp = *h;
+		*h = (*h)->next;
+		free(tmp);
+		for (j = 0; j <= i; j++)
+		{
+			if (*h == curr)
+			{
+				*h = NULL;
+				return (i);
+			}
+			curr = curr->next;
+		}
+	}
+	*h = NULL;
+	return (i);
 }

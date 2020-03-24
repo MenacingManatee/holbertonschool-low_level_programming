614c650b1baf02ca2797912740b6ea977c3aa943
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-24 23:13:17 +0000

    Removes dereference before NULL check and add check if head is NULL

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *tmp2, *tmp3;
 	unsigned int i = 0;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	tmp = *head;
 	if (index == 0)

5b1697e1f1fb8d30781598a3684b03e2ff093a73
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-24 23:21:10 +0000

    Adds extra NULL check, increases specificity of if checks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,20 +12,28 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *tmp2, *tmp3;
 	unsigned int i = 0;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (-1);
+
 	tmp = *head;
-	if (index == 0)
+
+	if (index == 0 && *head == NULL)
 	{
 		*head = tmp->next;
 		free(tmp);
 		return (1);
 	}
+
 	for (; tmp->next != NULL && i < index - 1; i++, tmp = tmp->next)
 		;
+
+	if (tmp == NULL)
+		return (-1);
+
 	tmp2 = tmp->next;
 	tmp3 = tmp2->next;
 	tmp->next = tmp3;
 	free(tmp2);
+
 	return (1);
 }

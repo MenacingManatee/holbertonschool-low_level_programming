f7e8a2a17180cb24529d8a05e16aba7d75a3c086
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-24 23:17:23 +0000

    Stops loop early if NULL is hit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	tmp = head;
-	for (; i < index; i++)
+	for (; tmp != NULL && i < index; i++)
 	{
 		tmp = tmp->next;
 	}

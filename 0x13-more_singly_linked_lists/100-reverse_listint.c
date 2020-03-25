8917296e0f8e34b3358a12331a21102a4fdcb666
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-25 01:23:13 +0000

    Adds a missing NULL check

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *tmp, *rev;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	tmp = (*head)->next;
 	(*head)->next = NULL;

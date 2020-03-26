7e9af3e7584358328f34dab1be5668898d6556b4
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-26 07:05:16 +0000

    Uses & operator to compare to a mask of the max size of a long unsigned

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,24 @@
+#include "holberton.h"
 #include <stdio.h>
-
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * print_binary - takes an int and prints it in binary
+ * @n: int to convert
+ * Return: void
  */
-int main(void)
+void print_binary(unsigned long int n)
 {
-	
-	return (0);
+	unsigned long int mask = 2147483648; /*1 then 31 0's*/
+	int flag = 0;
+
+	while (mask > 0)
+	{
+		if (mask & n)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag == 1 || !(mask >> 1))
+			_putchar('0');
+		mask >>= 1;
+	}
 }

ad3708a2a422425552966119cf28900210171004
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-26 21:25:51 +0000

    Adds a check for index being too large

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
-	if (n == NULL)
+	if (n == NULL || index > 31)
 		return (-1);
 	mask <<= index;
 	mask = mask | *n;

b89d07e286ef0885e548f07127a2296d62a87cd9
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-26 07:52:30 +0000

    Uses XOR operation to see the differences between bits, and a modified task 1 to tally different bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
+ * flip_bits - finds the number of bits needed to flip to get from n to m
+ * @n: number 1
+ * @m: number 2
  *
- * Return: Always 0 (ok)
+ * Return: Number of bits needed to flip
  */
-int main(void)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
-	return (0);
+	unsigned long int tmp = n^m, mask = 2147483648;
+	unsigned int t = 0;
+
+	while (mask > 0)
+	{
+		if (mask & tmp)
+			t++;
+		mask >>= 1;
+	}
+	return (t);
 }

543f5e5b501188922e82ddb6933fa93069980fb4
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-26 06:42:20 +0000

    Uses bitshifting to convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - 
- *
- * Return: Always 0 (ok)
+ * binary_to_uint - converts a binary string to decimal
+ * @b: binary string to convert
+ * Return: 0 on fail, converted num otherwise
  */
-int main(void)
+unsigned int binary_to_uint(const char *b)
 {
-	
-	return (0);
+	unsigned int sum = 0;
+
+	while (*b)
+	{
+		if (*b == '0' || *b == '1')
+		{
+			if (sum != 0)
+				sum <<= 1;
+			if (*b == '1')
+				sum++;
+		}
+		else
+			return (0);
+		b++;
+	}
+	return (sum);
 }

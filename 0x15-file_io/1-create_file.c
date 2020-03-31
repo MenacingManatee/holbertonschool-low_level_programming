a817537997b1a0298d3fd2d8d4409bfd68ac0453
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-31 07:17:55 +0000

    Adds a bunch of edge case checks, then writes to fd of potentially new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,28 @@
-#include <stdio.h>
+#include "holberton.h"
+#include <fcntl.h>
 
 /**
- * main - 
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write to the file
  *
- * Return: Always 0 (ok)
+ * Return: 1 on success, -1 on failure
  */
-int main(void)
+int create_file(const char *filename, char *text_content)
 {
-	
-	return (0);
+	int fd, len = 0, check;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	for (; text_content[len]; len++)
+		;
+	check = write(fd, text_content, len);
+	if (check != len)
+		return (-1);
+	return (1);
 }

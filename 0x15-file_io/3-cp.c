01d2d12ca8b45a413e4ac5e360212b2e89d46525
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-31 20:25:53 +0000

    Fixed successful write check, modified while loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #define RWRWR (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 #define ERR ("Error: Can't close fd %d\n")
+#define NOWRITE ("Error: Can't write to %s\n")
 /**
  * main - reads a text file and prints it to the POSIX stdout.
  * @argc: Argument count
@@ -13,7 +14,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bufflen = 1, tmp, close_fr, close_to;
+	int fd_from, fd_to, bufflen = 1, close_fr, close_to;
 	char buff[1024];
 
 	if (argc != 3)
@@ -31,17 +32,15 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, RWRWR);
 	if (fd_to < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, NOWRITE, argv[2]);
 		exit(99);
 	}
-	while (bufflen)
-	{
-		bufflen = read(fd_from, buff, 1024);
-		if (bufflen > 0)
+	while ((bufflen = read(fd_from, buff, 1024)) > 0)
+		if (write(fd_to, buff, bufflen))
 		{
-			tmp += write(fd_to, buff, bufflen);
+			dprintf(STDERR_FILENO, NOWRITE, argv[2]);
+			exit(99);
 		}
-	}
 	close_fr = close(fd_from);
 	close_to = close(fd_to);
 	if (close_fr < 0 || close_to < 0)

c54c84159c8e3aef299527895cfd72b9a54e6ea2
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-31 19:52:52 +0000

    Returns correct value if text content is NULL

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	for (; text_content[len]; len++)
 		;
 	check = write(fd, text_content, len);

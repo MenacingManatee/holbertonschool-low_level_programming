d3daeca591ede860909e5d123002e943a69f8f29
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-31 06:57:23 +0000

    Reads to a buffer, then writes and returns buff length

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
-
 /**
- * main - 
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: filename to look for
+ * @letters: Number of letters to read/print
  *
- * Return: Always 0 (ok)
+ * Return: 0 on failure, number of letters read/printed otherwise
  */
-int main(void)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	
-	return (0);
+	int filedesc, bufflen;
+	char buff[2048];
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	filedesc = open(filename, O_RDONLY);
+	if (filedesc < 0)
+		return (0);
+	if ((bufflen = read(filedesc, buff, letters)) > 0)
+	{
+		write(1, buff, bufflen);
+	}
+	return (bufflen);
 }

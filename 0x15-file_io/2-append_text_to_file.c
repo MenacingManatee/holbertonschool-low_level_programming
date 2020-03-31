74181e7cd0e7642bfc769866be9a739195fcc26a
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-31 07:23:27 +0000

    Minor modifications to 1 allow script to only append instead of create and truncate

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,28 @@
-#include <stdio.h>
+#include "holberton.h"
+#include <fcntl.h>
 
 /**
- * main - 
+ * append_text_to_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write to the file
  *
- * Return: Always 0 (ok)
+ * Return: 1 on success, -1 on failure
  */
-int main(void)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	
-	return (0);
+	int fd, len = 0, check;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+	for (; text_content[len]; len++)
+		;
+	check = write(fd, text_content, len);
+	if (check != len)
+		return (-1);
+	return (1);
 }

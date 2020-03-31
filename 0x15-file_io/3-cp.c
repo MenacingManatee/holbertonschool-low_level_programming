02b7dadc9c201c9c7e8fb086b515cf92c8438cc5
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-31 23:57:19 +0000

    Condensed a few lines and added a check if the read fails

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,7 @@
 #define RWRWR (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 #define ERR ("Error: Can't close fd %d\n")
 #define NOWRITE ("Error: Can't write to %s\n")
+#define NOREAD ("Error: Can't read from file %s\n")
 /**
  * main - reads a text file and prints it to the POSIX stdout.
  * @argc: Argument count
@@ -24,11 +25,7 @@ int main(int argc, char *argv[])
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from < 0)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, NOREAD, argv[1]), exit(98);
 	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, RWRWR);
 	if (fd_to < 0)
 	{
@@ -41,6 +38,8 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, NOWRITE, argv[2]);
 			exit(99);
 		}
+	if (bufflen < 0)
+		dprintf(STDERR_FILENO, NOREAD, argv[1]), exit(98);
 	close_fr = close(fd_from);
 	close_to = close(fd_to);
 	if (close_fr < 0)

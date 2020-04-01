c5d3c8bfaa890b783212085b9e07298b1ee86650
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-01 01:16:08 +0000

    Taking a break, pushing progress

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <elf.h>
 
+
+#define USAGE ("Usage: elf_header elf_filename\n")
+#define N_ELF ("Error: File must be an ELF file\n")
+void print_magic(Elf64_Ehdr *ehdr64);
 /**
- * main - 
+ * main - displays the information contained in the ELF header
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Return: Always 0 (ok)
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	if (argc != 3 || argv == NULL)
+	{
+		printf(USAGE);
+		exit(98);
+	}
 	return (0);
 }

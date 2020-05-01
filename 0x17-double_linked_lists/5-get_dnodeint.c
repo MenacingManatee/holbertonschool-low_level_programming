040a36024c9baf776d65bf2fee417bc7e5ab477a
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-01 07:29:30 +0000

    Moves to the start of the list, then moves down [index] nodes

--- 0x17-double_linked_lists/5-get_dnodeint.c
+++ 0x17-double_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,24 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: a dlistint_t linked list.
+ * @index: the node to return
  *
- * Return: Always 0 (ok)
+ * Return: the node at index [index]
  */
-int main(void)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	
-	return (0);
+	unsigned int i = 0;
+
+	if (!head)/*Null check*/
+		return (NULL);
+	if (!(head->prev))/*make sure we're actually at the head*/
+	{
+		for (; head->prev; head = head->prev)
+			;
+	}
+	for (; i < index && head; i++, head = head->next)
+		;
+	return (head);
 }

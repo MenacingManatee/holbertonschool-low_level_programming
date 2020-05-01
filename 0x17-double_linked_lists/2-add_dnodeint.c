30492b680cb0774b44635e32c19faaa9f73cccb8
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-01 06:34:42 +0000

    Includes a check for if you're actually at the head Removed h pointer so head outside function will be changed

--- 0x17-double_linked_lists/2-add_dnodeint.c
+++ 0x17-double_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,32 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: Head of the dlistint_t list
+ * @n: Contents of the node to be added
  *
- * Return: Always 0 (ok)
+ * Return: the address of the new element, or NULL if it failed
  */
-int main(void)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
-	return (0);
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if (!head || !*head)/*Null check*/
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	if (!((*head)->prev))/*make sure we're actually at the head*/
+	{
+		for (; (*head)->prev; *head = (*head)->prev)
+			;
+	}
+	new->next = *head;
+	*head = new;
+	return (new);
 }

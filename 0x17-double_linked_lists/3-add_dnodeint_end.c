97545b8187fdb1bca8a2f33fcb7c5119ea571012
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-01 07:09:06 +0000

    Similar to the last one, but moves to the end instead of the start

--- 0x17-double_linked_lists/3-add_dnodeint_end.c
+++ 0x17-double_linked_lists/3-add_dnodeint_end.c
@@ -1,12 +1,31 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: head of a dlistint_t list.
+ * @n: contents of dlistint_t list to be created
  *
- * Return: Always 0 (ok)
+ * Return: the address of the new element, or NULL if it failed
  */
-int main(void)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	
-	return (0);
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *tmp;
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!head || !*head)/*Null check*/
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	for (; (*head)->next; *head = (*head)->next)
+		;/*move to the end of the list*/
+	new->prev = *head;
+	(*head)->next = new;
+	*head = tmp;
+	return (new);
 }

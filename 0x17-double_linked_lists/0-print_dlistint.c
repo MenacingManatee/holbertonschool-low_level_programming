1818430b699f34461edd30daaf4f994762aa6f55
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-01 06:14:15 +0000

    Adds a check for if we're actually at the head

--- 0x17-double_linked_lists/0-print_dlistint.c
+++ 0x17-double_linked_lists/0-print_dlistint.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * print_dlistint - prints a doubly linked list
+ * @h: head of the dlintint
  *
- * Return: Always 0 (ok)
+ * Return: The number of nodes
  */
-int main(void)
+size_t print_dlistint(const dlistint_t *h)
 {
-	
-	return (0);
+	int nodes;
+
+	if (!h)/*Null check*/
+		return (0);
+	if (!(h->prev))/*make sure we're actually at the head*/
+	{
+		for (; h->prev; h = h->prev)
+			;
+	}
+	for (nodes = 0; h; h = h->next, nodes++)
+		printf("%d\n", h->n);
+	return (nodes);
 }

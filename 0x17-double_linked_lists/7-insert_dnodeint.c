f20d4fe8bbc948f5b56aaa66cbc4b294d9b256b4
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-01 07:45:19 +0000

    Saving progress: Basic functionality works, inserts node at incorrect position

--- 0x17-double_linked_lists/7-insert_dnodeint.c
+++ 0x17-double_linked_lists/7-insert_dnodeint.c
@@ -1,12 +1,45 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: a pointer to a dlistint_t linked list.
+ * @idx: the position to add the node
+ * @n: The data contained in the new node
  *
- * Return: Always 0 (ok)
+ * Return: the address of the new node, or NULL if it failed
  */
-int main(void)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	
-	return (0);
+	unsigned int i = 0;
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *top, *next;
+
+	if (!new)/*malloc failure check*/
+		return (NULL);
+	if (!h || !*h)/*Null check*/
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	if (!((*h)->prev))/*make sure we're actually at the head*/
+	{
+		for (; (*h)->prev; (*h) = (*h)->prev)
+			;
+	}
+	top = *h;
+	for (; i < idx && (*h); i++, (*h) = (*h)->next)
+		;
+	if (!*h)
+	{
+		free(new);
+		return (NULL);
+	}
+	next = (*h)->next;
+	(*h)->next = new;
+	new->prev = *h;
+	new->next = next;
+	*h = next;
+	(*h)->prev = new;
+	*h = top;
+	return (new);
 }

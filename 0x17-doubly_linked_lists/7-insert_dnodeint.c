11b6a091e82071a545380f55c1b0ed43f8bed3d7
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-04 01:33:23 +0000

    Adds missing pointer

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,8 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		else
 		{
 			*h = new;
-			new->next = NULL;
-			new->prev = NULL;
+			new->next = NULL, new->prev = NULL;
 		}
 	}
 	new->n = n;

3882789986896955056554c1a6fe72ccc0a69558
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-04 00:53:00 +0000

    Note to self: test code before pushing

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	if (!head)
 		return (NULL);
-	if!*head)/*Null check*/
+	if (!*head)/*Null check*/
 	{
 		new->next = NULL;
 		*head = new;

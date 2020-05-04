d3322344386fd3dd35963fc1c62eb43cf1cfcb00
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-04 00:54:10 +0000

    Reverting previous change

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,9 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	if (!head)
-		return (NULL);
-	if (!*head)/*Null check*/
+	if (!head || !*head)/*Null check*/
 	{
 		new->next = NULL;
 		*head = new;

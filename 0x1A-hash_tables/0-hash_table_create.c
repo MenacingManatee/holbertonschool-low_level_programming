e5fe42c9c7ff602f53b3261895d9dac9d890ef56
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-28 22:34:33 +0000

    Uses calloc to ensure everything is initialized to 0/NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,30 @@
+#include "hash_tables.h"
 #include <stdio.h>
 
 /**
- * main - 
+ * hash_table_create - creates an empty hash table of size
+ * @size: size of the hash table to create
  *
- * Return: Always 0 (ok)
+ * Return: A pointer to the created hash table
  */
-int main(void)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	
-	return (0);
+	hash_table_t *res = calloc(1, sizeof(hash_table_t));
+	hash_node_t *head;
+
+	if (res == NULL)
+		return (NULL);
+	if (size == 0)
+	{
+		free(res);
+		return (NULL);
+	}
+	head = calloc(size, sizeof(hash_node_t));
+	if (head == NULL)
+	{
+		free(res);
+		return (NULL);
+	}
+	res->size = size, res->array = &head;
+	return (res);
 }

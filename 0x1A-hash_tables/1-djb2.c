22cac809ff4d09fbb2bab8ce90e945e2f741d04a
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-28 22:39:11 +0000

    Is it really just copy pasting the provided function? That seems wrong

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
+#include "hash_tables.h"
 
 /**
- * main - 
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
- * Return: Always 0 (ok)
+ * Return: hash value
  */
-int main(void)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-	
-	return (0);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }

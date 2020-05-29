7282ac8855f849eb304dfa776bb0e4238da2e292
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-29 21:48:48 +0000

    Adds a handle for pre-existing keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,6 +29,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		for (tmp = head[idx]; tmp->next != NULL; tmp = tmp->next)
+		{
+			if (!strcmp(tmp->key, key))
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				return (1);
+			}
+		}
 		tmp = create_node(key, value);
 		tmp->next = head[idx];
 		head[idx] = tmp;

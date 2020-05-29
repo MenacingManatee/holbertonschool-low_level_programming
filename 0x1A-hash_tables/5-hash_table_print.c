dd4526a8df26b0251b3833a7e4f1a7a03e7df0d4
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-29 22:33:24 +0000

    Fixed print formatting

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,12 +1,35 @@
+#include "hash_tables.h"
 #include <stdio.h>
 
 /**
- * main - 
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table
  *
- * Return: Always 0 (ok)
+ * Return: void
  */
-int main(void)
+void hash_table_print(const hash_table_t *ht)
 {
-	
-	return (0);
+	unsigned long int i, flag = 0;
+	hash_node_t **run, *tmp;
+
+	if (!ht)
+		return;
+	run = ht->array;
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		if (run[i])
+		{
+			if (flag == 1)
+				putchar(',');
+			for (tmp = run[i]; tmp != NULL; tmp = tmp->next)
+			{
+				if (flag == 1)
+					putchar(' ');
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				flag = 1;
+			}
+		}
+	}
+	printf("}\n");
 }

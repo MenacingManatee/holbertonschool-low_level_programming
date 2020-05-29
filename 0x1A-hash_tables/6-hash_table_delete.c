ce921efb4fcc5c7159afb19a9c928538b54e1c72
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-29 23:49:38 +0000

    Loops through the table same as in task 5, deleting everything instead of printing

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,12 +1,46 @@
+#include "hash_tables.h"
 #include <stdio.h>
 
+void free_node(hash_node_t *node);
 /**
- * main - 
+ * hash_table_delete - frees a hash table.
+ * @ht: the hash table
  *
- * Return: Always 0 (ok)
+ * Return: void
  */
-int main(void)
+void hash_table_delete(hash_table_t *ht)
 {
-	
-	return (0);
+	unsigned long int i;
+	hash_node_t **run, *tmp, *curr;
+
+	if (!ht)
+		return;
+	run = ht->array;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (run[i])
+		{
+			for (tmp = run[i]; tmp != NULL;)
+			{
+				curr = tmp;
+				tmp = tmp->next;
+				free_node(curr);
+			}
+		}
+	}
+	free(ht->array);
+	free((void *)ht);
+}
+
+/**
+ * free_node - frees a node
+ * @node: node to free
+ *
+ * Return: void
+ */
+void free_node(hash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
 }

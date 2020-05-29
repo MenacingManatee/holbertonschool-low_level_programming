360fc9b029be6589ec9a19a097ed7a39c3723596
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-29 03:50:12 +0000

    Taking a break, saving progress. Current version I believe works unless there's a collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,50 @@
-#include <stdio.h>
+#include "hash_tables.h"
+#include <string.h>
 
 /**
- * main - 
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key to add
+ * @value: value associated with key
  *
- * Return: Always 0 (ok)
+ * Return: 1 on success, 0 on failure
  */
-int main(void)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
-	return (0);
+	unsigned long int k, idx, i;
+	hash_node_t **head;
+
+	if (!ht || !key || key[0] == '\0')
+		return (0);
+	k = hash_djb2((unsigned char *)key);
+	idx = k % ht->size;
+	head = ht->array;
+	if (!head[idx])
+	{
+		head[idx] = malloc(sizeof(hash_node_t));
+		if (!head[idx])
+			return (0);
+		(head[idx])->key = malloc(strlen(key));
+		if (!(head[idx])->key)
+		{
+			free(head[idx]);
+			head[idx] = NULL;
+			return (0);
+		}
+		strcpy((head[idx])->key, key);
+		if (!value)
+		{
+			(head[idx])->value = malloc(strlen(value));
+			if (!(head[idx])->value)
+				return (0);
+			strcpy((head[idx])->value, value);
+		}
+		(head[idx])->next = (head[idx + 1]);
+	}
+	else
+	{
+		for (i = 0; i < ht->size && !head[i]; i++)
+			;
+		
+	return (1);
 }

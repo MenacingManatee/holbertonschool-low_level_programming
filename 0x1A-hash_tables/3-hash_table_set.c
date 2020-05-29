f015d438c9ae04e41491320153db35871ec5eb5c
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-29 04:28:10 +0000

    It worked until I removed the debug text...

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,8 @@
 #include "hash_tables.h"
 #include <string.h>
+#include <stdio.h>
 
+hash_node_t *create_node(const char *key, const char *value);
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table
@@ -19,32 +21,55 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	k = hash_djb2((unsigned char *)key);
 	idx = k % ht->size;
 	head = ht->array;
-	if (!head[idx])
+	if (head[idx] == NULL)
 	{
-		head[idx] = malloc(sizeof(hash_node_t));
+		head[idx] = create_node(key, value);
 		if (!head[idx])
 			return (0);
-		(head[idx])->key = malloc(strlen(key));
-		if (!(head[idx])->key)
-		{
-			free(head[idx]);
-			head[idx] = NULL;
-			return (0);
-		}
-		strcpy((head[idx])->key, key);
-		if (!value)
-		{
-			(head[idx])->value = malloc(strlen(value));
-			if (!(head[idx])->value)
-				return (0);
-			strcpy((head[idx])->value, value);
-		}
-		(head[idx])->next = (head[idx + 1]);
 	}
 	else
 	{
-		for (i = 0; i < ht->size && !head[i]; i++)
+		for (i = 0; i < ht->size && head[i]; i++)
 			;
-		
+		if (head[i])
+			return (0);
+		head[i] = create_node(key, value);
+		if (head[i] == NULL)
+			return (0);
+		(head[i])->next = head[i + 1];
+	}
 	return (1);
+}
+
+/**
+ * create_node - creates a node
+ *
+ *
+ * Return: The created node
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (!node)
+		return (0);
+	node->key = malloc(strlen(key) + 1);
+	if (!node->key)
+	{
+		free(node);
+		return (0);
+	}
+	strcpy(node->key, key);
+	if (value != NULL)
+	{
+		node->value = malloc(strlen(value) + 1);
+		if (!node->value)
+		{
+			free(node->key);
+			free(node);
+			return (NULL);
+		}
+		strcpy((node->value), value);
+	}
+	return (node);
 }

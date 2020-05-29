893e0d2a8f45d7327452914fd47cb684617dc11a
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-29 22:02:20 +0000

    Loop now actually checks the last node for duplicates

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		for (tmp = head[idx]; tmp->next != NULL; tmp = tmp->next)
+		for (tmp = head[idx]; tmp != NULL; tmp = tmp->next)
 		{
 			if (!strcmp(tmp->key, key))
 			{
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,40 @@
+#include "hash_tables.h"
+#include <string.h>
 #include <stdio.h>
 
 /**
- * main - 
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
  *
- * Return: Always 0 (ok)
+ * Return: the value associated with the element, or NULL if key isn't found
  */
-int main(void)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	
-	return (0);
+	unsigned long int k, i;
+	hash_node_t **tmp;
+
+	printf("Bugcheck\n");
+	k = hash_djb2((unsigned char *)key) % ht->size;
+	tmp = ht->array;
+
+	printf("%d\n", tmp[k] == NULL);
+	printf("Before if\n");
+	if (tmp[k] != NULL)
+	{
+		printf("Inside if: %s : %s\n", tmp[k]->key, tmp[k]->value);
+		if (tmp[k]->key != NULL)
+			if (!strcmp(key, tmp[k]->key))
+				return (tmp[k]->value);
+	}
+	printf("Before loop\n");
+	for (i = 0; i < ht->size; i++)
+	{
+		printf("%d\n", (int)i);
+		if (tmp[i] != NULL)
+			if (tmp[i]->key != NULL)
+				if (!strcmp(key, tmp[i]->key))
+					return (tmp[i]->value);
+	}
+	return (NULL);
 }

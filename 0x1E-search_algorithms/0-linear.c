738745ee8dccfe199a8e775b86d58ee08b6e7878
Brett Davis <brett.p.davis101@gmail.com>
2020-08-25 12:25:25 +0000

    Adds testfiles dir Updates header file with required structs adds 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,24 @@
-#include 
+#include "search_algos.h"
 
 /**
- * main - 
+ * linear_search - searches for a value in an array of integers using the
+ * Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: size of the array
+ * @value: value to search for
  *
- * Return: Always 0 (ok)
+ * Return: Index of value, or -1 if not found
  */
-int main(void)
+int linear_search(int *array, size_t size, int value)
 {
-	
-	return (0);
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n",
+		       (int) i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }

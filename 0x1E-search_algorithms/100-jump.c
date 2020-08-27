b4ad201f80185e179ae342968bdc672b8a04136f
Brett Davis <brett.p.davis101@gmail.com>
2020-08-27 07:52:22 +0000

    Uses an if/elif/else chain to decide what to print and if to do the linear search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,12 +1,62 @@
-#include <stdio.h>
+#include "search_algos.h"
+#include <math.h>
 
 /**
- * main - 
+ * linear - searches for a value in a sorted array of integers
+ * @array: array to be searched
+ * @start: Starting index
+ * @end: ending index
+ * @value: value to be searched for
  *
- * Return: Always 0 (ok)
+ * Return: index of number, or -1
  */
-int main(void)
+int linear(int *array, size_t start, size_t end, int value)
 {
-	
-	return (0);
+	size_t i;
+
+	for (i = start; i <= end; i++)
+	{
+		printf("Value checked array [%d] = [%d]\n", (int)i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search
+ * @size: number of elements in arraynumber of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 on failure
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jump, start, i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	jump = sqrt(size);
+	for (i = start = 0; i < size; i += jump)
+	{
+		if (array[i] >= value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n",
+			       (int)start, (int)i);
+			return (linear(array, start, i, value));
+		}
+		else if (i + jump >= size)
+		{
+			printf("Value checked array[%d] = [%d]\n", (int)i,
+			       array[i]);
+			printf("Value found between indexes [%d] and [%d]\n",
+			       (int)i, (int)(i + jump));
+			return (linear(array, i, size - 1, value));
+		}
+		else
+			printf("Value checked array[%d] = [%d]\n", (int)i,
+			       array[i]);
+		start = i;
+	}
+	return (-1);
 }
